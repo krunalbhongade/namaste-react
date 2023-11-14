5541a8a5d084fbcaf385fce89cba86b52a792ece
krunalbhongade <krunal.bhongade@gmail.com>
2023-11-14 16:53:05 +0530

    useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Body from "./Body";
 
 
 
+
  
 
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Restaurantcard from "./Restaurantcard";
 import resobject from "./utils/mockData";
+import Shimmer from "./utils/Shimmer";
 
 const Body = () => {
-   
-    const [listOfRestaurants, updateList] = useState(resobject);
-    let a = 10;
+    const [listOfRestaurants, updateList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://dummyjson.com/products"
+        );
+
+        const json = await data.json();
+        console.log(json);
+        //setTimeout(()=>{updateList(resobject);console.log('timeout')}, 3000);
+        updateList(resobject);
+    };
+
+    if (listOfRestaurants.length === 0) {
+        return <h1><Shimmer /></h1>;
+    }
+
     return (
         <div className="body">
             <div className="filter">
                 <button
                     className="filter-btn"
                     onClick={() => {
-                        updateList(listOfRestaurants.filter(
-                            (res) => res.info.rating.aggregate_rating > 4
-                        ));
+                        updateList(
+                            listOfRestaurants.filter(
+                                (res) => res.info.rating.aggregate_rating > 4
+                            )
+                        );
                         a = 20;
                         console.log(listOfRestaurants);
                     }}
